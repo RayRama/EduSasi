47d3b7d6035c2efdd895e20848192b9e57641d22
RayRama <rayramadita12@gmail.com>
2019-07-20 05:22:50 +0700

    initial commit

---
+++ app/src/main/java/com/smanegeri1sindang/edusasi/News/NewsMainActivity.java
@@ -0,0 +1,16 @@
+package com.smanegeri1sindang.edusasi.News;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.smanegeri1sindang.edusasi.R;
+
+public class NewsMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_main);
+    }
+}
